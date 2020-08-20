4aa5efef6e3bf066e60dcd09a86e1da0ccfeb3e1
Aravind <39290472+aravindvinas@users.noreply.github.com>
2020-08-21 00:08:35 +0530

    Update hx711.c

--- HX711/hx711.c
+++ HX711/hx711.c
@@ -1,44 +1,86 @@
 #include "hx711.h"
 
-uint32_t load_val_read(hx711_Typedef *hx711)
+
+hx711_typedef hx711_init(GPIO_TypeDef *port_dout, GPIO_TypeDef *port_sck, uint16_t dout, uint16_t sck, uint8_t channel_gain)
+{
+	hx711_typedef config;
+
+	config.dout = port_dout;
+	config.sck = port_sck;
+	config.pin_dout = dout;
+	config.pin_sck = sck;
+	config.offset = 0;
+	config.calibration = 1;
+	config.gain = channel_gain;
+
+	return config;
+}
+
+float get_weight(hx711_typedef *hx)
 {
     int load_val = 0;
 
-    //Wait until ready state
-    while(HAL_GPIO_ReadPin(hx711->dout, hx711->dout_pin) == 1);
+    while (HAL_GPIO_ReadPin(hx->dout, hx->pin_dout)==1);
 
-    for(int i=0; i<24; i++)
+    for (uint8_t i = 0; i < 24; i++)
     {
-        HAL_GPIO_WritePin(hx711->sck, hx711->sck_pin, GPIO_PIN_SET);
+    	HAL_GPIO_WritePin(hx->sck, hx->pin_sck, GPIO_PIN_SET);
 
-        //if DOUT reads 1
-        if(HAL_GPIO_ReadPin(hx711->dout, hx711->dout_pin) == 1){
-            load_val = load_val << 1;
-            load_val |= 0x00000001; 
+        if (HAL_GPIO_ReadPin(hx->dout, hx->pin_dout))
+        {
+        	load_val = load_val << 1;
+        	load_val |= 0x00000001;
         }
 
-        //if DOUT reads 0
-        else{
-            load_val = load_val << 1;
-            load_val &= 0xFFFFFFFE; 
+        else
+        {
+        	load_val = load_val << 1;
+        	load_val &= 0xFFFFFFFE;
         }
 
-        HAL_GPIO_WritePin(hx711->sck, hx711->sck_pin, GPIO_PIN_RESET);
+    	HAL_GPIO_WritePin(hx->sck, hx->pin_sck, GPIO_PIN_RESET);
     }
 
-    //Setting gain value for next conv
-    for(int i=0; i<hx711->channel_gain; i++)
+    for (uint8_t i=0; i<hx->gain; i++)
     {
-        HAL_GPIO_WritePin(hx711->sck, hx711->sck_pin, GPIO_PIN_SET);
-        HAL_Delay(1);
-        HAL_GPIO_WritePin(hx711->sck, hx711->sck_pin, GPIO_PIN_RESET);
+    	HAL_GPIO_WritePin(hx->sck, hx->pin_sck, GPIO_PIN_SET);
+    	HAL_GPIO_WritePin(hx->sck, hx->pin_sck, GPIO_PIN_RESET);
     }
 
-    //Bitwise XOR to get absolute value
     load_val = load_val ^ 0x800000;
 
-    //return measured load value
+    load_val = float(load_val);
+    load_val = load_val - hx->offset;
+    load_val = load_val/hx->calibration;
+
     return load_val;
 }
 
+void tare_calc(hx711_typedef *hx)
+{
+	float tare = 0;
+	uint8_t i = 0;
 
+	while(i<16){
+		tare = tare + get_weight(hx);
+		i++;
+	}
+
+	tare = tare/16;
+	hx->offset = tare;
+}
+
+void calibrate(hx711_typedef *hx, uint32_t actual_mass)
+{
+	//get weight before calibration is set
+	float raw = float(get_weight);
+
+	//new calibration value
+	float new_cal = 0;
+
+	//calculating new calibration
+	new_cal = old_cal*raw;	
+	new_cal = new_cal/actual_mass;	
+
+	hx->calibration = new_cal;
+}
