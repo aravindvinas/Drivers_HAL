8d7d4eea665f8d99e57ad46c006acdaaffaf498f
Aravind <39290472+aravindvinas@users.noreply.github.com>
2020-08-21 14:29:18 +0530

    Update hx711.c

--- HX711/hx711.c
+++ HX711/hx711.c
@@ -1,5 +1,29 @@
 #include "hx711.h"
 
+static uint8_t first_flag = 0;
+static uin32_t data_set[16];
+
+static float mov_win_avg(uint32_t new_val)
+{
+    //moving window average
+    for(uint8_t i=1; i<15; i++)
+    {
+        data_set[i-1] = data_set[i];
+    }
+
+    data_set[15] = new_val;
+
+    for(uint8_t i=0; i<16; i++)
+    {
+        new_val = new_val + data_set[i];
+    }
+
+    new_val = new_val/16;
+    new_val = new_val - hx.offset;
+    new_val = (float)new_val/(hx.calibration);
+
+    return new_val;
+}
 
 hx711_typedef hx711_init(GPIO_TypeDef *port_dout, GPIO_TypeDef *port_sck, uint16_t dout, uint16_t sck, uint8_t channel_gain)
 {
@@ -16,9 +40,9 @@ hx711_typedef hx711_init(GPIO_TypeDef *port_dout, GPIO_TypeDef *port_sck, uint16
 	return config;
 }
 
-float get_weight(hx711_typedef hx)
+static uint32_t probe_weight(hx711_typedef hx)
 {
-    int load_val = 0;
+    uint32_t load_val = 0;
 
     while (HAL_GPIO_ReadPin(hx.dout, hx.pin_dout)==1);
 
@@ -49,27 +73,50 @@ float get_weight(hx711_typedef hx)
 
     load_val = load_val ^ 0x800000;
 
-    load_val = (float)load_val;
-    load_val = load_val - hx.offset;
-    load_val = load_val/(hx.calibration);
-
     return load_val;
+}
+
+float get_weight(hx711_typedef hx)
+{
+    uint32_t probe = 0;
+    float val;
+    if(!flag)
+    {
+        flag = 1;
+
+        //populate data_set first
+        for(uint8_t i=0; i<16; i++)
+        {
+            data_set[i] = probe_weight(hx);
+        }
+
+        probe = probe_weight(hx);
+        val = mov_win_avg(probe);
+    }
+
+    else
+    {
+        probe = probe_weight(hx);
+        val = mov_win_avg(probe);
+    }
+
+    return val;
 }
 
 hx711_typedef tare_calc(hx711_typedef hx)
 {
-	float tare = 0;
+	uint32_t tare = 0;
 	uint8_t i = 0;
 
 	while(i<16){
-		tare = tare + get_weight(hx);
+		tare = tare + probe_weight(hx);
 		i++;
 	}
 
 	tare = tare/16;
 	hx.offset = tare;
 
-        return hx;
+    return hx;
 }
 
 hx711_typedef calibrate(hx711_typedef hx, float actual_mass)
@@ -86,5 +133,5 @@ hx711_typedef calibrate(hx711_typedef hx, float actual_mass)
 
 	hx.calibration = new_cal;
         
-        return hx;
+    return hx;
 }
