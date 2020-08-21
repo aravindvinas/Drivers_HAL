7dcaa766723f3361db74adbc4e752264513b82fc
Aravind <39290472+aravindvinas@users.noreply.github.com>
2020-08-21 12:08:15 +0530

    Update hx711.h

--- HX711/hx711.h
+++ HX711/hx711.h
@@ -11,36 +11,36 @@
  */
  
 #ifndef __HX711_H
-#define	__HX711_H
+#define __HX711_H
 
 #include "stm32f1xx_hal.h"
 
 // Channel and gain configuration
-#define CHANNEL_A_128	0x01
-#define CHANNEL_A_64	0x03
-#define CHANNEL_B_32	0x02
+#define CHANNEL_A_128   0x01
+#define CHANNEL_A_64    0x03
+#define CHANNEL_B_32    0x02
 
 // HX711 configuration struct
 typedef struct 
 {
-	GPIO_TypeDef *dout;
-	GPIO_TypeDef *sck;
-	uint16_t pin_dout;
-	uint16_t pin_sck;
-	float offset;
-	float calibration;
-	uint8_t gain;
+    GPIO_TypeDef *dout;
+    GPIO_TypeDef *sck;
+    uint16_t pin_dout;
+    uint16_t pin_sck;
+    float offset;
+    float calibration;
+    uint8_t gain;
 } hx711_typedef;
 
 hx711_typedef hx711_init(GPIO_TypeDef *port_dout, 
-			 GPIO_TypeDef *port_sck, 
-			 uint16_t dout, 
-			 uint16_t sck, 
-			 uint8_t channel_gain);
+             GPIO_TypeDef *port_sck, 
+             uint16_t dout, 
+             uint16_t sck, 
+             uint8_t channel_gain);
 
-float get_weight(hx711_typedef *hx);
-void tare_calc(hx711_typedef *hx);
-void calibrate(hx711_typedef *hx, uint32_t actual_mass);
+float get_weight(hx711_typedef hx);
+hx711_typedef tare_calc(hx711_typedef hx);
+hx711_typedef calibrate(hx711_typedef hx, float actual_mass);
 
 
 #endif
