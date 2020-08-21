16a119c0f8c438513462914ae9aa9be76da0ef05
Aravind <39290472+aravindvinas@users.noreply.github.com>
2020-08-21 12:07:54 +0530

    Update hx711.c

--- HX711/hx711.c
+++ HX711/hx711.c
@@ -16,17 +16,17 @@ hx711_typedef hx711_init(GPIO_TypeDef *port_dout, GPIO_TypeDef *port_sck, uint16
 	return config;
 }
 
-float get_weight(hx711_typedef *hx)
+float get_weight(hx711_typedef hx)
 {
     int load_val = 0;
 
-    while (HAL_GPIO_ReadPin(hx->dout, hx->pin_dout)==1);
+    while (HAL_GPIO_ReadPin(hx.dout, hx.pin_dout)==1);
 
     for (uint8_t i = 0; i < 24; i++)
     {
-    	HAL_GPIO_WritePin(hx->sck, hx->pin_sck, GPIO_PIN_SET);
+    	HAL_GPIO_WritePin(hx.sck, hx.pin_sck, GPIO_PIN_SET);
 
-        if (HAL_GPIO_ReadPin(hx->dout, hx->pin_dout))
+        if (HAL_GPIO_ReadPin(hx.dout, hx.pin_dout))
         {
         	load_val = load_val << 1;
         	load_val |= 0x00000001;
@@ -38,25 +38,25 @@ float get_weight(hx711_typedef *hx)
         	load_val &= 0xFFFFFFFE;
         }
 
-    	HAL_GPIO_WritePin(hx->sck, hx->pin_sck, GPIO_PIN_RESET);
+    	HAL_GPIO_WritePin(hx.sck, hx.pin_sck, GPIO_PIN_RESET);
     }
 
-    for (uint8_t i=0; i<hx->gain; i++)
+    for (uint8_t i=0; i<hx.gain; i++)
     {
-    	HAL_GPIO_WritePin(hx->sck, hx->pin_sck, GPIO_PIN_SET);
-    	HAL_GPIO_WritePin(hx->sck, hx->pin_sck, GPIO_PIN_RESET);
+    	HAL_GPIO_WritePin(hx.sck, hx.pin_sck, GPIO_PIN_SET);
+    	HAL_GPIO_WritePin(hx.sck, hx.pin_sck, GPIO_PIN_RESET);
     }
 
     load_val = load_val ^ 0x800000;
 
-    load_val = float(load_val);
-    load_val = load_val - hx->offset;
-    load_val = load_val/hx->calibration;
+    load_val = (float)load_val;
+    load_val = load_val - hx.offset;
+    load_val = load_val/(hx.calibration);
 
     return load_val;
 }
 
-void tare_calc(hx711_typedef *hx)
+hx711_typedef tare_calc(hx711_typedef hx)
 {
 	float tare = 0;
 	uint8_t i = 0;
@@ -67,20 +67,24 @@ void tare_calc(hx711_typedef *hx)
 	}
 
 	tare = tare/16;
-	hx->offset = tare;
+	hx.offset = tare;
+
+        return hx;
 }
 
-void calibrate(hx711_typedef *hx, uint32_t actual_mass)
+hx711_typedef calibrate(hx711_typedef hx, float actual_mass)
 {
 	//get weight before calibration is set
-	float raw = float(get_weight);
+	float raw = get_weight(hx);
 
 	//new calibration value
 	float new_cal = 0;
 
 	//calculating new calibration
-	new_cal = old_cal*raw;	
+	new_cal = (hx.calibration)*raw;	
 	new_cal = new_cal/actual_mass;	
 
-	hx->calibration = new_cal;
+	hx.calibration = new_cal;
+        
+        return hx;
 }
